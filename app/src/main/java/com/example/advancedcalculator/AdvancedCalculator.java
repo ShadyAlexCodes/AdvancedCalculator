68f899890af2cbfdef15b6d132351d71b2c225fc
Xander Endre <45239432+ShadyAlexCodes@users.noreply.github.com>
2021-11-01 12:29:49 -0600

    Adding checks for buttons

---
+++ app/src/main/java/com/example/advancedcalculator/AdvancedCalculator.java
@@ -0,0 +1,15 @@
+package com.example.advancedcalculator;
+
+import java.lang.reflect.Array;
+
+public class AdvancedCalculator {
+
+    private int inputNumber;
+    private int storageNumber;
+
+
+    public void setInputNumber(int inputNumber) {
+
+        this.inputNumber = inputNumber;
+    }
+}
--- app/src/main/java/com/example/advancedcalculator/MainActivity.java
+++ app/src/main/java/com/example/advancedcalculator/MainActivity.java
@@ -5,7 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.lang.reflect.Array;
+
 public class MainActivity extends AppCompatActivity {
+
+    AdvancedCalculator advancedCalculator = new AdvancedCalculator();
+
+    private Array numberStorage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +21,69 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void getUserOperation(View view) {
+        switch(view.getId()) {
+            case R.id.operationClear:
+                System.out.println("CLEAR");
+                break;
+            case R.id.operationModulus:
+                System.out.println("MODULUS");
+                break;
+            case R.id.operationMultiplication:
+                System.out.println("MULTIPLICATION");
+                break;
+            case R.id.operationDivision:
+                System.out.println("DIVISION");
+                break;
+            case R.id.operationSubtraction:
+                System.out.println("SUBTRACTION");
+                break;
+            case R.id.operatorAddition:
+                System.out.println("ADDITION");
+                break;
+            case R.id.operatorEqual:
+                System.out.println("EQUAL");
+                break;
+            default:
+                System.out.println("THERE WAS AN ERROR");
+                break;
+        }
+    }
 
+    public void getUserNumber(View view) {
+        switch(view.getId()) {
+            case R.id.btnZero:
+                System.out.println("ZERO");
+                break;
+            case R.id.btnOne:
+                System.out.println("ONE");
+                break;
+            case R.id.btnTwo:
+                System.out.println("TWO");
+                break;
+            case R.id.btnThree:
+                System.out.println("THREE");
+                break;
+            case R.id.btnFour:
+                System.out.println("FOUR");
+                break;
+            case R.id.btnFive:
+                System.out.println("FIVE");
+                break;
+            case R.id.btnSix:
+                System.out.println("SIX");
+                break;
+            case R.id.btnSeven:
+                System.out.println("SEVEN");
+                break;
+            case R.id.btnEight:
+                System.out.println("EIGT");
+                break;
+            case R.id.btnNine:
+                System.out.println("NINE");
+                break;
+            default:
+                System.out.println("THERE WAS AN ERROR");
+                break;
+        }
     }
 }
