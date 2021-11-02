d553aa6c6d9338007fbf44c9e8207957e37b54e1
Xander Endre <45239432+ShadyAlexCodes@users.noreply.github.com>
2021-11-02 12:59:16 -0600

    FINALIZED PROJECT

--- app/src/main/java/com/example/advancedcalculator/MainActivity.java
+++ app/src/main/java/com/example/advancedcalculator/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.advancedcalculator;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -46,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
     AdvancedCalculator advancedCalculator = new AdvancedCalculator();
 
     // Create a text view to store the output and error message
-    TextView txtOutput, txtErrorMsg;
+    TextView txtOutput, txtWarning, txtPlaceholder;
     // Create a string to store the current operation
     String currentOperation;
 
@@ -62,12 +63,13 @@ public class MainActivity extends AppCompatActivity {
 
         // Define the output and error message box.
         txtOutput = findViewById(R.id.txtOutput);
-        txtErrorMsg = findViewById(R.id.txtError);
+        txtWarning = findViewById(R.id.txtWarning);
+        txtPlaceholder = findViewById(R.id.txtPlaceholder);
     }
 
 
     public void getUserOperation(View view) {
-        // Define a pusdeo variable that'll act as the placeholder for multiple ID's
+        // Define a psuedo variable that'll act as the placeholder for multiple ID's
         Button button = (Button) view;
 
         // Remove any errors
@@ -80,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
         currentOperation = button.getText().toString();
 
         // Set the error message text for visual purposes
-        txtErrorMsg.setText("A: " + firstNumber + " " + currentOperation + " " + txtOutput.getText().toString());
+        txtPlaceholder.setText(firstNumber + " " + currentOperation + " " + txtOutput.getText().toString());
     }
 
     public void getUserNumber(View view) {
@@ -93,8 +95,9 @@ public class MainActivity extends AppCompatActivity {
     public void clearButton(View view) {
         // Clear everything
 
-        txtOutput.setText("");
-        txtErrorMsg.setText("");
+        txtOutput.setText(null);
+        txtWarning.setText(null);
+        txtPlaceholder.setText(null);
         firstNumber = Double.NaN;
         secondNumber = 0;
         currentOperation = null;
@@ -110,34 +113,64 @@ public class MainActivity extends AppCompatActivity {
         currentOperation = null;
     }
 
+    private boolean endsWithOperator() {
+        return txtWarning.getText().toString().contains("+") ||
+                txtWarning.getText().toString().contains("-") ||
+                txtWarning.getText().toString().contains("/") ||
+                txtWarning.getText().toString().contains("x") ||
+                txtWarning.getText().toString().contains("%");
+    }
+
+    public void decimalNoWork(View view) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                txtPlaceholder.setText(null);
+            }
+        }, 4000);
+        txtPlaceholder.setText("hahahaha u funny. this button no work");
+
+
+    }
 
     private void calculateExpression() {
-        // Check if the double is not-not a number
-        if (!Double.isNaN(firstNumber)) {
 
-            // Assign the second number the value of the text output
-            secondNumber = Double.parseDouble(txtOutput.
-                    getText().toString());
+        if (!endsWithOperator()) {
+            // Check if the double is not-not a number
+            if (!Double.isNaN(firstNumber)) {
 
-            // Reset the output
-            txtOutput.setText(null);
+                // Assign the second number the value of the text output
+                secondNumber = Double.parseDouble(txtOutput.
+                        getText().toString());
 
-            // Preform the equation
-            txtOutput.setText(Double.toString(advancedCalculator.operation(firstNumber, secondNumber, currentOperation)));
-        } else {
-            // try parsing the first number
-            try {
-                // Set the first number equal to the doubled parse value
-                firstNumber = Double.parseDouble(txtOutput.getText().toString());
-                // Set the output text to null
+                // Reset the output
                 txtOutput.setText(null);
-            } catch (Exception e) {
+                txtPlaceholder.setText(null);
+                // Preform the equation
+                txtOutput.setText(Double.toString(advancedCalculator.operation(firstNumber, secondNumber, currentOperation)));
+            } else {
+                // try parsing the first number
+                try {
+                    // Set the first number equal to the doubled parse value
+                    firstNumber = Double.parseDouble(txtOutput.getText().toString());
+                    // Set the output text to null
+                    txtOutput.setText(null);
+                } catch (Exception e) {
 
-                // set the text color to red
-                txtErrorMsg.setTextColor(Color.RED);
-                // inform the user they are having number issues.
-                txtErrorMsg.setText("Please enter some numbers to continue.");
+                    // set the text color to red
+                    txtWarning.setTextColor(Color.RED);
+                    // inform the user they are having number issues.
+                    txtWarning.setText("Please enter some numbers to continue.");
+                }
             }
+        } else
+        {
+            
+            // set the text color to red
+            txtWarning.setTextColor(Color.RED);
+            // inform the user they are having number issues.
+            txtWarning.setText("Please enter some numbers to continue.");
+            System.out.println("ERROR");
         }
     }
 
@@ -148,12 +181,12 @@ public class MainActivity extends AppCompatActivity {
             // Appends the number to the string
             txtOutput.append(Integer.toString(number));
             // Displays the initial number
-            txtErrorMsg.append(Integer.toString(number));
+            txtPlaceholder.append(Integer.toString(number));
             System.out.println("OUTPUT: " + txtOutput.getText().toString());
         } else {
             // Display red and inform the user that they cannot enter more than 10 numbers.
-            txtErrorMsg.setTextColor(Color.RED);
-            txtErrorMsg.setText("You cannot enter more than 10 numbers");
+            txtWarning.setTextColor(Color.RED);
+            txtWarning.setText("You cannot enter more than 10 numbers");
         }
     }
 }
